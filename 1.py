a935037d46a65d5c41b0f7dfbd85145a0a45692f
alves-Moises <moisesinho_ra@hotmail.com>
2023-04-19 02:03:50 -0300

    exercise 1

---
+++ 1.py
@@ -0,0 +1,37 @@
+
+valid = False
+while not valid:
+    #recebe o valor e valida o tipo inteiro
+    try:
+        valor = int(input("DIgite o valor de K:"))
+    except ValueError:
+        print("Digite um valor inteiro!")
+    else:
+        if(valor <= 1):
+            print("Digite um valor maior que 1!\n")
+        else:
+            valid = True  #muda valor da variável de controle caso o valor de entrada seja um número inteiro
+
+valid = False
+while not valid:
+    # pergunta e valida triangulo par ou impar
+    triangulo = input("Digite \"i\" para Triângulo impar ou \"p\" para triangulo par: ").lower()
+
+    if((triangulo != "i") and (triangulo != "p")):   
+        print("Resposta inválida!\n")
+    else:
+        valid = True
+    
+if(triangulo == "i"):
+    n = 1
+else:
+    n = 2
+
+
+produto = 1
+for i in range(valor):
+    produto *= n
+    n += 2
+    print(produto, n)
+
+print(produto)
